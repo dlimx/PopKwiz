756c3e7d05fb4dc1d9ae74976b5ea6a20401803b
Justin Lin <linjaron@gmail.com>
2021-01-22 11:56:02 -0800

    Login functionality implemented
    Now able to log into previously created account.

--- client/screens/Login.jsx
+++ client/screens/Login.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import React, { useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+// import { Button } from '@material-ui/core';
+import { useAuth } from '../store/users/AuthContext';
 
-export const Login = () => {
+export function Login() {
+  const email = useRef('');
+  const password = useRef('');
+  const { login } = useAuth()
+  const history = useHistory();
+
+  // Login using firebase authentication
+  async function handleLogin(e) {
+    try {
+      e.preventDefault();
+      let userInfo = await login(email.current.value, password.current.value);
+      console.log(userInfo.user.uid);
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      <h1>Page coming soon</h1>
-      <Button variant="contained" color="primary" component={Link} to="/">
-        Home
-      </Button>
-    </div>
+    // TODO: Create better UI for login using MaterialUI
+    <form onSubmit={handleLogin}>
+      <h1>Log In Page:</h1>
+      <label>
+        Email:
+        <input type="text" name="username" ref={email}/>
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" name="password" ref={password}/>
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+
+    // <div>
+    //   <h1>Page coming soon</h1>
+
+    //   <Button variant="contained" color="primary" component={Link} to="/">
+    //     Home
+    //   </Button>
+    // </div>
   );
 };
