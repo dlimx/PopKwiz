ad997daa971d76c85adf8d105d1b0c709da65714
David Li <me@davidli.io>
2021-01-31 20:47:34 -0800

    Add categories endpoint

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRouter } from './users/routes';
 import { quizRouter } from './quizzes/routes';
+import { categoryRouter } from './categories/routes';
 import { signupRouter } from './users/signupRoute';
 import { authMiddleWare } from './utils/authMiddleWare';
 
@@ -11,3 +12,4 @@ apiRouter.use('/signup', signupRouter);
 apiRouter.use('/users', authMiddleWare);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/quizzes', quizRouter);
+apiRouter.use('/categories', categoryRouter);
