d70cb25a21588cbec520faff59a334246def2eb2
davidlee0602 <david.lee0602@gmail.com>
2021-01-31 21:51:20 -0500

    implementing browse API.

--- client/screens/Browse.jsx
+++ client/screens/Browse.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Search } from '../components/Search';
 import { Category } from '../components/Category';
 import { Quizlist } from '../components/Quizlist';
 import { Copyright } from '../components/Copyright';
 
-export const Browse = () => (
-  <div>
-    <Search />
-    <Category />
-    <Quizlist />
-    <Copyright />
-  </div>
-);
+// page to browse quizzes - with collection of search modules
+export const Browse = () => {
+  const [searchVal, setSearchVal] = useState('');
+  const [categoryVal, setCategoryVal] = useState('');
+  const [quizList, setQuizList] = useState([]);
+  useEffect(() => {
+    axios.get(`/api/quizzes?search=${searchVal}&category=${categoryVal}`).then((res) => {
+      setQuizList(res.data);
+    });
+  }, [searchVal, categoryVal]);
+
+  return (
+    <div>
+      <Search setSearchVal={setSearchVal} />
+      <Category setCategoryVal={setCategoryVal} />
+      <Quizlist quizList={quizList} />
+      <Copyright />
+    </div>
+  );
+};
--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userRouter } from './users/routes';
+import { quizRouter } from './quizzes/routes';
 
 export const apiRouter = express.Router();
 
 apiRouter.use('/users', userRouter);
+apiRouter.use('/quizzes', quizRouter);
