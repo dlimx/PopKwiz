4de05481832fa4204668591c3e6e177a3b82781d
davidlee0602 <david.lee0602@gmail.com>
2021-03-02 18:47:42 -0500

    rebase prep

--- client/screens/QuizProfile.jsx
+++ client/screens/QuizProfile.jsx
@@ -4,36 +4,38 @@ import { QuizComments } from '../components/QuizComments';
 import { UserComment } from '../components/UserComment';
 import { useAPI } from '../api/api';
 
-
 export const QuizProfile = () => {
-    const api = useAPI();
-    const { id } = useParams();
-    const [quizComments, setQuizComments] = useState({});
+  const api = useAPI();
+  const { id } = useParams();
+  const [quizComments, setQuizComments] = useState({});
 
-    // state to track star rating by user
-    const [rateVal, setRate] = useState(-1);
+  // state to track star rating by user
+  const [rateVal, setRate] = useState(-1);
 
-    // state to track user comment value in text field
-    const [commentVal, setComment] = useState('');
+  // state to track user comment value in text field
+  const [commentVal, setComment] = useState('');
 
-    const [editVal, setEdit] = useState(0);
+  const [editVal, setEdit] = useState(0);
 
-    const loadQuiz = async (quizID) => {
-        api.get(`/quizzes/${quizID}`).then((res) => {
-            setQuizComments(res.data.data.rating);
-        });
-      };
-    
-      useEffect(() => {
-        loadQuiz(id);
-      }, [editVal]);
-      
+  useEffect(() => {
+    api.get(`/quizzes/${id}`).then((res) => {
+      setQuizComments(res.data.data.rating);
+    });
+  }, [api, id, editVal]);
 
-
-    return (
-        <div>
-            <UserComment quizID={id} quizComments={quizComments} rateVal={rateVal} commentVal={commentVal} setRate={setRate} setComment={setComment} editVal={editVal} setEdit = {setEdit}/>
-            <QuizComments quizComments={quizComments}/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <UserComment
+        quizID={id}
+        quizComments={quizComments}
+        rateVal={rateVal}
+        commentVal={commentVal}
+        setRate={setRate}
+        setComment={setComment}
+        editVal={editVal}
+        setEdit={setEdit}
+      />
+      <QuizComments quizComments={quizComments} />
+    </div>
+  );
+};
