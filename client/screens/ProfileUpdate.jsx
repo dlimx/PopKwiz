90df89ba2251eed0180bdc549f40655e77eac4bf
David Vega <35232697+ddvega@users.noreply.github.com>
2021-03-05 20:34:26 -0600

    Vega avatar2 (#41)
    * upload and download avatar working. Need to figure out how to get the url to refresh when the picture is updated. It only shows up after cache storage is cleared
    
    * upload and download of avatar working without bugs
    
    * ui changes to profile update

--- client/screens/ProfileUpdate.jsx
+++ client/screens/ProfileUpdate.jsx
@@ -1,98 +1,222 @@
-import React, { useState } from 'react';
-import PersonIcon from '@material-ui/icons/Person';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useCallback, useRef } from 'react';
+import { Button, Typography, Accordion, Container, AccordionDetails, Grid, TextField } from '@material-ui/core';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { withStyles } from '@material-ui/core/styles';
 import { useAuth } from '../store/users/AuthContext';
-import { FormBuilder } from '../components/FormBuilder';
+import { ImageUploadPreview } from '../components/ImageUploadPreview';
+import { useAPI } from '../api/api';
+import { AlertMessage } from '../components/AlertMessage';
+
+const AccordionSummary = withStyles({
+  root: {
+    flexDirection: 'column',
+  },
+  content: {
+    marginBottom: 0,
+  },
+  expandIcon: {
+    marginRight: 0,
+    paddingTop: 0,
+  },
+})(MuiAccordionSummary);
 
 export function ProfileUpdate() {
-  const { currentUser, updatePassword, updateEmail } = useAuth();
-  const [email, setEmail] = useState(currentUser.email);
+  const { updatePassword, updateEmail } = useAuth();
+  const [status, setStatusBase] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confPassword, setconfPassword] = useState('');
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const history = useHistory();
+  const [confPassword, setConfPassword] = useState('');
+  const [image, setImage] = useState('');
+  const api = useAPI();
 
+  // update username
+  const handleUserName = (value) => {
+    setUsername(value);
+  };
+
+  const handleUserUpdate = async (e) => {
+    e.preventDefault();
+    localStorage.removeItem('username');
+    localStorage.setItem('username', username);
+    try {
+      api.post('/users/username', { username }).then((res) => {
+        console.log(res);
+      });
+      setStatusBase({ msg: 'Success: User has been updated', key: Math.random() });
+    } catch (error) {
+      localStorage.removeItem('username');
+      setStatusBase({ msg: error, key: Math.random() });
+    }
+  };
+
+  // update email
   const handleEmail = (value) => {
     setEmail(value);
   };
+
+  const handleEmailUpdate = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('email');
+    localStorage.setItem('email', email);
+    try {
+      api.post('/users/email', { email }).then((res) => {
+        console.log(res);
+      });
+      updateEmail(email);
+      setStatusBase({ msg: 'Success: Email has been updated', key: Math.random() });
+    } catch (error) {
+      localStorage.removeItem('email');
+      setStatusBase({ msg: error, key: Math.random() });
+    }
+  };
+
+  // update password
   const handlePassword = (value) => {
     setPassword(value);
   };
+
   const handleConfPassword = (value) => {
-    setconfPassword(value);
+    setConfPassword(value);
   };
 
-  // eslint-disable-next-line consistent-return
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (password !== confPassword) {
-      return setError('Passwords do not match');
+  const handlePasswordUpdate = (e) => {
+    if (password === confPassword) {
+      updatePassword(password);
+      setStatusBase({ msg: 'SUCCESS: Password updated.', key: Math.random() });
+    } else {
+      setStatusBase({ msg: 'ERROR: Passwords do not match', key: Math.random() });
     }
+  };
 
-    const promises = [];
-    setLoading(true);
-    setError('');
-
-    if (email !== currentUser.email) {
-      promises.push(updateEmail(email));
-    }
-    if (password) {
-      promises.push(updatePassword(password));
-    }
-
-    Promise.all(promises)
-      .then(() => {
-        history.push('/');
-      })
-      .catch(() => {
-        setError('Failed to update account');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }
+  // update user image
+  const uploadAvatar = useCallback(
+    (data) => {
+      let body;
+      try {
+        if (image) {
+          localStorage.removeItem('picture');
+          body = new FormData();
+          body.append('image', image);
+          api.post('/users/picture', body).then((res) => {
+            localStorage.setItem('picture', res.data);
+          });
+        } else {
+          body = data;
+        }
+        setStatusBase({ msg: 'SUCCESS: Image updated.', key: Math.random() });
+      } catch (error) {
+        setStatusBase({ msg: 'ERROR: Failure uploading image to database.', key: Math.random() });
+        localStorage.removeItem('picture');
+      }
+    },
+    [api, image],
+  );
 
   return (
-    <FormBuilder
-      header="Update Profile"
-      fields={[
-        {
-          name: 'email',
-          label: 'new email',
-          type: 'email',
-          id: 'email',
-          autoComplete: 'email',
-          value: email,
-          onChange: handleEmail,
-        },
-        {
-          name: 'password',
-          label: 'new password',
-          type: 'password',
-          id: 'password',
-          autoComplete: 'password',
-          value: password,
-          onChange: handlePassword,
-        },
-        {
-          name: 'confpassword',
-          label: 'confirm new password',
-          type: 'password',
-          id: 'confpassword',
-          autoComplete: 'confpassword',
-          value: confPassword,
-          onChange: handleConfPassword,
-        },
-      ]}
-      buttons={[
-        {
-          text: 'Submit',
-          onClick: handleSubmit,
-        },
-      ]}
-    >
-      <PersonIcon />
-    </FormBuilder>
+    <>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Update Profile Picture</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container maxWidth="xs">
+            <ImageUploadPreview image={image} setImage={setImage} />
+            <Button color="primary" variant="contained" onClick={uploadAvatar}>
+              UPDATE
+            </Button>
+            {status ? <AlertMessage key={status.key} message={status.msg} /> : null}
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Update Username</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container maxWidth="xs">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  label="new username"
+                  onChange={(e) => handleUserName(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+            <Button color="primary" variant="contained" onClick={handleUserUpdate}>
+              UPDATE
+            </Button>
+            {status ? <AlertMessage key={status.key} message={status.msg} /> : null}
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Update Email</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container maxWidth="xs">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  type="email"
+                  required
+                  fullWidth
+                  label="new email"
+                  onChange={(e) => handleEmail(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+            <Button color="primary" variant="contained" onClick={handleEmailUpdate}>
+              UPDATE
+            </Button>
+            {status ? <AlertMessage key={status.key} message={status.msg} /> : null}
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Update Password</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container maxWidth="xs">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  type="password"
+                  required
+                  fullWidth
+                  label="new password"
+                  onChange={(e) => handlePassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  type="password"
+                  required
+                  fullWidth
+                  label="confirm new password"
+                  onChange={(e) => handleConfPassword(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+            <Button color="primary" variant="contained" onClick={handlePasswordUpdate}>
+              UPDATE
+            </Button>
+            {status ? <AlertMessage key={status.key} message={status.msg} /> : null}
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+    </>
   );
 }
